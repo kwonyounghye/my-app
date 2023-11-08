83cb0496399f925556ccc8c3013dca244eb929ce
kwonyounghye <kyh90721@gmail.com>
2023-11-08 12:02:08 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let isrc = 'https://ik.imagekit.io/ikmedia/backlit.jpg';
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+        <img src={isrc} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
+          style={{borderRadius:"50%"}}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          리액트
         </a>
       </header>
     </div>
